46994192320ecc15579c5a0b3508f3abf17f90b3
Stetson King <Stetson.e.king@outlook.com>
2024-09-10 19:12:24 -0400

    skeleton for svelte

---
+++ activity2/part1.js
@@ -0,0 +1,21 @@
+// 1A
+// function addTwoNumbers(a, b){
+//     return a+b; 
+// }
+
+let addTwoNumbers = (a, b) => a + b;
+
+// 1B
+// function stringLength( myStr ){
+//     if(myStr.length < 10)
+//         return "short";
+//     return "long";
+// }
+
+let stringLength = (myStr) => myStr.length < 10 ? "short" : "long";
+
+// 2
+// let fn = (a,b) => { a>b ? console.log(a) : console.log(b) }
+
+// If a is greater than b, print a to console else print b to console
+
